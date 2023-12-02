b409d88a175a6aa2d12d4881e39f50a496740cb0
Jaco van Gelder <jacovangelder@MacBook-Pro-van-Jaco-2.local>
2023-12-02 17:48:23 +0100

    try deploy storage

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,73 @@
-module "adls" {
-  source      = "./modules/adls"
-  environment = var.environment
+# --------------------------
+# General Input
+# --------------------------
+
+locals {
+  project_name = "dtype"
+  common_tags = {
+    Now_Location    = "NL"
+    Now_Environment = var.environment
+    ApplicationName = local.project_name
+    ModifiedAt      = timestamp()
+  }
 }
+
+# --------------------------
+# Data blocks
+# --------------------------
+
+data "azurerm_client_config" "current" {
+}
+
+# --------------------------
+# Resource Groups
+# --------------------------
+
+resource "azurerm_resource_group" "storage_rg" {
+  name     = module.naming_storage.resource_group.name
+  location = var.location
+  tags     = local.common_tags
+}
+
+# --------------------------
+# Modules
+# --------------------------
+
+# Naming
+module "naming" {
+  source = "./modules/utilities/naming"
+  suffix = [local.project_name, var.environment]
+}
+
+# Naming Storage
+module "naming_storage" {
+  source = "./modules/utilities/naming"
+  suffix = ["st", local.project_name, var.environment]
+}
+
+module "naming_key_vault" {
+  source = "./modules/utilities/naming"
+  suffix = ["kv", local.project_name, var.environment]
+}
+
+# Storage
+module "adls" {
+  source               = "./modules/adls"
+  environment          = var.environment
+  resource_group_name  = azurerm_resource_group.storage_rg.name
+  location             = var.location
+  storage_account_name = module.naming.storage_account.name
+}
+
+# Key Vault
+# module "key_vault" {
+#   source   = "./modules/key-vault"
+#   location = var.location
+#   #tags                                 = var.tags
+#   key_vault_resource_group_name        = azurerm_resource_group.key_vault_rg.name
+#   key_vault_sku_name                   = "standard"
+#   key_vault_name                       = module.naming.key_vault.name
+#   key_vault_soft_delete_retention_days = "90"
+#   key_vault_tenant_id                  = data.azurerm_client_config.current.tenant_id
+#   # log_analytics_workspace_id           = module.log_analytics.log_analytics_workspace_id
+# }
---
+++ infra/modules/adls/utilities/naming/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"sort"
+	"text/template"
+)
+
+// Resource definition for the package
+type Resource struct {
+	Name   string  `json:"name"`
+	Length *Length `json:"length,omitempty"`
+	Regex  *string `json:"regex,omitempty"`
+	Scope  *string `json:"scope,omitempty"`
+	Slug   *string `json:"slug,omitempty"`
+	Dashes bool    `json:"dashes"`
+}
+
+// Length allowed for that resorce
+type Length struct {
+	Min int `json:"min"`
+	Max int `json:"max"`
+}
+
+func main() {
+	files, err := ioutil.ReadDir("templates")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var fileNames = make([]string, len(files))
+	for i, file := range files {
+		fileNames[i] = "templates/" + file.Name()
+	}
+	parsedTemplate, err := template.New("templates").Funcs(template.FuncMap{
+		// Terraform not yet support lookahead in their regex function
+		"cleanRegex": func(dirtyString string) string {
+			var re = regexp.MustCompile(`(?m)\(\?=.{\d+,\d+}\$\)|\(\?!\.\*--\)`)
+			return re.ReplaceAllString(dirtyString, "")
+		},
+	}).ParseFiles(fileNames...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sourceDefinitions, err := ioutil.ReadFile("resourceDefinition.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data []Resource
+	err = json.Unmarshal(sourceDefinitions, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Sort the documented and undocumented resources alphabetically
+	sort.Slice(data, func(i, j int) bool {
+		return data[i].Name < data[j].Name
+	})
+
+	mainFile, err := os.OpenFile("main.tf", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	parsedTemplate.ExecuteTemplate(mainFile, "main", data)
+	outputsFile, err := os.OpenFile("outputs.tf", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	parsedTemplate.ExecuteTemplate(outputsFile, "outputs", data)
+}
