f86ca539b65a0f8f9941061f4fc6a084aff65b15
Jaco van Gelder <jacovangelder@MacBook-Pro-van-Jaco-2.local>
2023-12-03 19:56:36 +0100

    try mount

--- infra/main.tf
+++ infra/main.tf
@@ -100,7 +100,7 @@ module "key_vault" {
 }
 
 resource "azurerm_key_vault_access_policy" "spn_kv_admin_access_policies" {
-  key_vault_id = azurerm_key_vault.keyvault.id
+  key_vault_id = module.key_vault.key_vault_id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azuread_service_principal.service_principal.object_id
 
