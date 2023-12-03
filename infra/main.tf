ac54e744b1757aa86b4474245ada4278b77a6ed3
Jaco van Gelder <jacovangelder@MacBook-Pro-van-Jaco-2.local>
2023-12-03 21:27:43 +0100

    uc metastore

--- infra/main.tf
+++ infra/main.tf
@@ -49,6 +49,13 @@ resource "azurerm_resource_group" "databricks_rg" {
   tags     = local.common_tags
 }
 
+resource "azurerm_resource_group" "unity_catalog_metastore_rg" {
+  count    = var.environment == "prd" ? 1 : 0
+  name     = "rg-metastore-dtype"
+  location = var.location
+  tags     = local.common_tags
+}
+
 # --------------------------
 # Modules
 # --------------------------
@@ -188,4 +195,42 @@ resource "databricks_mount" "mounting_filesystems" {
     module.databricks_workspace,
     module.adls
   ]
+}
+
+# Unity Catalog (only deploy once on prd since you can only have one metastore in one region)
+resource "azurerm_storage_account" "unity_catalog_metastore_storage" {
+  count                    = length(azurerm_resource_group.unity_catalog_metastore_rg)
+  resource_group_name      = azurerm_resource_group.unity_catalog_metastore_rg[0].name
+  name                     = "stdtypeucmetastore"
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "ZRS"
+  tags                     = local.common_tags
+
+  account_kind   = "StorageV2"
+  is_hns_enabled = true
+  access_tier    = "Hot"
+
+  enable_https_traffic_only = true
+  min_tls_version           = "TLS1_2"
+
+  depends_on = [module.databricks_workspace]
+}
+
+resource "azurerm_storage_container" "unity_catalog" {
+  count                = length(azurerm_resource_group.unity_catalog_metastore_rg)
+  name                 = "metastore"
+  storage_account_name = azurerm_storage_account.unity_catalog_metastore_storage[count.index].name
+}
+
+resource "databricks_metastore" "metastore" {
+  count = length(azurerm_resource_group.unity_catalog_metastore_rg)
+  name  = "dtype-store-we"
+  storage_root = format("abfss://%s@%s.dfs.core.windows.net/",
+    azurerm_storage_container.unity_catalog[0].name,
+    azurerm_storage_account.unity_catalog_metastore_storage[0].name)
+  owner         = "contact@innovion.nl"
+  region        = "westeurope"
+  force_destroy = true
+  depends_on    = [module.databricks_workspace]
 }
